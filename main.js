04c804019c3092674386f173d43c3b31ba427d69
Rejor <rejorjiao@gmail.com>
2021-07-20 01:41:04 +0900

    small changes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 gsap.from('.intro-animation',{
     duration: 1, opacity:0, y: 50, stagger:0.4, ease: "back.out(1.7)", ease: "power2"
 })
+
 gsap.to("em",{
     delay: 1, duration: 1, color:"#535353", stagger: 0.2, ease: "power2"
 })
