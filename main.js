f61ac721d3fc6176039206e97d6055288a2737b4
Rejor <rejorjiao@gmail.com>
2021-07-20 21:24:43 +0900

    improve load animation

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ window.addEventListener("load", function(){
     console.log("loaded")
     const loader = document.querySelector(".loader")
     loader.classList.add("loader-fade")
-})
 
-gsap.from('.intro-animation',{
-    duration: 1, opacity:0, y: 50, stagger:0.4, ease: "back.out(1.7)", ease: "power2"
-})
+    gsap.from('.intro-animation',{
+        duration: 1, opacity:0, y: 50, stagger:0.4, ease: "back.out(1.7)", ease: "power2"
+    })
 
-gsap.to("em",{
-    delay: 1, duration: 1, color:"#535353", stagger: 0.2, ease: "power2"
+    gsap.to("em",{
+        delay: 1, duration: 1, color:"#535353", stagger: 0.2, ease: "power2"
+    })
 })
