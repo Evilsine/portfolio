346ee4bbca51da4e2fde144613acfbd0a05078a4
Rejor <rejorjiao@gmail.com>
2021-07-20 21:42:00 +0900

    updated loading screen

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ window.addEventListener("load", function(){
     loader.classList.add("loader-fade")
 
     gsap.from('.intro-animation',{
-        duration: 1, opacity:0, y: 50, stagger:0.4, ease: "back.out(1.7)", ease: "power2"
+        delay: 1, duration: 1, opacity:0, y: 50, stagger:0.4, ease: "back.out(1.7)", ease: "power2"
     })
 
     gsap.to("em",{
-        delay: 1, duration: 1, color:"#535353", stagger: 0.2, ease: "power2"
+        delay: 2, duration: 1, color:"#535353", stagger: 0.2, ease: "power2"
     })
 })
